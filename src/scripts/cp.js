63b996a09dc061c67437cf37e63330fdb6dd89ef
rider_griffin <rider.s.griffin@gmail.com>
2021-10-27 14:34:54 -0700

    resets state object entirely

--- src/scripts/cp.js
+++ src/scripts/cp.js
@@ -151,7 +151,7 @@ CoursePresentation.prototype.constructor = CoursePresentation;
  * @return {object}
  */
 CoursePresentation.prototype.getCurrentState = function () {
-  var state = this.previousState ? Object.assign({}, this.previousState) : {};
+  var state = {};
   state.progress = this.getCurrentSlideIndex();
   if (!state.answers) {
     state.answers = [];
